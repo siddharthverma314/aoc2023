3bd2e34526fdfc99ef51c88e6619b3e737fad313
Siddharth Verma <siddharthverma314@gmail.com>
2024-01-20 01:31:59 -0500

    day 1 part 1

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          uiua
+          alejandra
+          nil
+        ];
+      };
+    });
+}
